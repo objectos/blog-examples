544ab4c4256d8950eb9c41ccfd9ea46edcccb4a1
Marcio Endo <marcio@objectos.com.br>
2022-06-17 14:38:18 -0300

    www: work array list blog post
    $source=194ef931befabee0ae84d2c99bc1f19c3edeb021$

---
+++ p008-array-backed-list/src/main/java/iter1/FixedLength.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2022 Objectos Software LTDA.
+ *
+ * Reprodução parcial ou total proibida.
+ */
+package iter1;
+
+public class FixedLength {
+  public static void main(String[] args) {
+    var abc = new String[3];
+
+    var i = 0;
+
+    abc[i++] = "A";
+    abc[i++] = "B";
+    abc[i++] = "C";
+    abc[i++] = "D";
+  }
+}
