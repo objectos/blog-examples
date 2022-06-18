6707d76815edd1f97d1222e2f8c82aaeb1d840ac
Marcio Endo <marcio@objectos.com.br>
2022-06-18 10:37:37 -0300

    www: work array list blog post
    $source=a8ea9f9e464e546eeb3b87b3e6d1ca2b51cf0706$

---
+++ p008-array-backed-list/src/main/java/iter1/JOLArraySize.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2022 Objectos Software LTDA.
+ *
+ * Reprodução parcial ou total proibida.
+ */
+package iter1;
+
+import static java.lang.System.out;
+
+import org.openjdk.jol.info.ClassLayout;
+import org.openjdk.jol.vm.VM;
+
+public class JOLArraySize {
+  public static void main(String[] args) {
+    out.println(VM.current().details());
+
+    out.println("String[1000]");
+    out.println(ClassLayout.parseInstance(new String[1000]).toPrintable());
+
+    out.println("int[1000]");
+    out.println(ClassLayout.parseInstance(new int[1000]).toPrintable());
+  }
+}
