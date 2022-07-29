2fb855e72e0f8d6c3899c8fda50747866d523156
Marcio Endo <marcio@objectos.com.br>
2022-07-29 14:28:46 -0300

    work switch blog post
    $source=6e0ee3d9c0f15f055cb71c26dac43b50e521674f$

---
+++ p013-switch/src/main/java/post/TableSwitch.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2022 Objectos Software LTDA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package post;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+
+@State(Scope.Thread)
+public class TableSwitch {
+
+  @Param({"1", "2", "3", "4", "5", "6", "7", "8", "1024"})
+  int state;
+
+  @Benchmark
+  public int benchmark() {
+    return switch (state) {
+      case 1 -> 1001;
+      case 2 -> 1002;
+      case 3 -> 1003;
+      case 4 -> 1004;
+      case 5 -> 1005;
+      case 6 -> 1006;
+      case 7 -> 1007;
+      case 8 -> 1008;
+      default -> Integer.MAX_VALUE;
+    };
+  }
+
+}
