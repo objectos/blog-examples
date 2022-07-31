59b173e0e97adf1d8dcfdc63715ba28009d1f321
Marcio Endo <marcio@objectos.com.br>
2022-07-31 14:41:07 -0300

    work switch blog post
    $source=5e1515df2d3ddf8e617890437a0c2fd538c2564a$

--- p013-switch/src/main/java/post/Benchmarks.java
+++ p013-switch/src/main/java/post/Benchmarks.java
@@ -15,9 +15,6 @@
  */
 package post;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
@@ -32,29 +29,20 @@ import org.openjdk.jmh.annotations.Warmup;
 @State(Scope.Thread)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
-@Warmup(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 3, time = 1, timeUnit = TimeUnit.SECONDS)
-public class Switch {
-
+@Warmup(time = 2, timeUnit = TimeUnit.SECONDS)
+@Measurement(time = 2, timeUnit = TimeUnit.SECONDS)
+public class Benchmarks {
   int[] lookup;
-
   int[] table;
-
   int x = 123;
 
   @Setup
-  public void _shuffle() {
+  public void _setup() {
     lookup = new int[] {
-        1, 5, 10, 50, 100,
-
-        500, 1000, 5000, 10000, 50000
+        257, 68, 13, 16, 529, 528,
+        144, 146, 32, 96, 160, 162
     };
-
-    Collections.shuffle(Arrays.asList(lookup));
-
-    table = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-    Collections.shuffle(Arrays.asList(table));
+    table = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12};
   }
 
   @Benchmark
@@ -65,16 +53,18 @@ public class Switch {
       var state = lookup[i];
 
       result += switch (state) {
-        case 1 -> compute(1001);
-        case 5 -> compute(1002);
-        case 10 -> compute(1003);
-        case 50 -> compute(1004);
-        case 100 -> compute(1005);
-        case 500 -> compute(1006);
-        case 1000 -> compute(1007);
-        case 5000 -> compute(1008);
-        case 10000 -> compute(1009);
-        case 50000 -> compute(1010);
+        case 257 -> compute(1001);
+        case 68 -> compute(1002);
+        case 13 -> compute(1003);
+        case 16 -> compute(1004);
+        case 529 -> x;
+        case 528 -> compute(1006);
+        case 144 -> compute(1007);
+        case 146 -> compute(1008);
+        case 32 -> 1234;
+        case 96 -> compute(1010);
+        case 160 -> state;
+        case 162 -> state;
         default -> 0;
       };
     }
@@ -94,12 +84,14 @@ public class Switch {
         case 2 -> compute(1002);
         case 3 -> compute(1003);
         case 4 -> compute(1004);
-        case 5 -> compute(1005);
+        case 5 -> x;
         case 6 -> compute(1006);
         case 7 -> compute(1007);
         case 8 -> compute(1008);
-        case 9 -> compute(1009);
+        case 9 -> 1234;
         case 10 -> compute(1010);
+        case 11 -> state;
+        case 12 -> state;
         default -> 0;
       };
     }
@@ -108,7 +100,6 @@ public class Switch {
   }
 
   private int compute(int value) {
-    return ThreadLocalRandom.current().nextInt(value * x);
+    return value * x;
   }
-
 }
---
+++ p013-switch/src/main/java/post/States.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2022 Objectos Software LTDA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package post;
+
+public class States {
+  private static final int MAYBE = 1 << 0;
+  private static final int START = MAYBE;
+
+  private static final int NL = 1 << 1;
+
+  private static final int DOCUMENT = 1 << 2;
+
+  private static final int METADATA = 1 << 3;
+
+  private static final int PREAMBLE = 1 << 4;
+
+  private static final int SECTION = 1 << 5;
+
+  private static final int HEADING = 1 << 6;
+
+  private static final int PARAGRAPH = 1 << 7;
+
+  private static final int ATTR = 1 << 8;
+
+  private static final int LISTING_BLOCK = 1 << 9;
+
+  public static void main(String[] args) {
+    System.out.println(MAYBE | ATTR);
+
+    System.out.println(DOCUMENT | HEADING);
+
+    System.out.println(MAYBE | DOCUMENT | METADATA);
+
+    System.out.println(PREAMBLE);
+
+    System.out.println(PREAMBLE | LISTING_BLOCK | START);
+
+    System.out.println(PREAMBLE | LISTING_BLOCK);
+
+    System.out.println(PREAMBLE | PARAGRAPH);
+
+    System.out.println(PREAMBLE | PARAGRAPH | NL);
+
+    System.out.println(SECTION);
+
+    System.out.println(SECTION | HEADING);
+
+    System.out.println(SECTION | PARAGRAPH);
+
+    System.out.println(SECTION | PARAGRAPH | NL);
+  }
+}
