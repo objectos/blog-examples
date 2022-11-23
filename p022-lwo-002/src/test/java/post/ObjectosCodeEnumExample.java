ee07ee46719f3f037b20a1ba05cbb3b381657bf2
Marcio Endo <marcio@objectos.com.br>
2022-11-23 09:08:46 -0300

    public lwo 002 post
    $source=eb698a8e9f2264d37324e72dace1ec4e3fe55b17$

---
+++ p022-lwo-002/src/test/java/post/ObjectosCodeEnumExample.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2022 Objectos Software LTDA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package post;
+
+import objectos.code.ClassName;
+import objectos.code.JavaTemplate;
+import objectos.code.PackageName;
+
+public class ObjectosCodeEnumExample extends JavaTemplate {
+
+  public static void main(String[] args) {
+    var ex = new ObjectosCodeEnumExample();
+    System.out.println(ex.toString());
+  }
+
+  @Override
+  protected final void definition() {
+    var test = PackageName.of("test");
+    var iface = ClassName.of(test, "Iface");
+
+    _enum(
+      _public(), id("Test"), _implements(iface),
+      enumConstant(id("A"), s("a")),
+      enumConstant(id("B"), s("b")),
+      field(_private(), _final(), t(String.class), id("value")),
+      constructor(
+        _private(),
+        param(t(String.class), id("value")),
+        assign(n(_this(), "value"), n("value"))
+      ),
+      method(
+        annotation(Override.class),
+        _public(), _final(), t(String.class), id("toString"),
+        _return(n("value"))
+      )
+    );
+  }
+
+}
