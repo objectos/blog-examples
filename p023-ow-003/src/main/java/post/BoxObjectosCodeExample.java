6a6523e6edf3fe9c6a813a123ed129ad4f5a240c
Marcio Endo <marcio@objectos.com.br>
2022-11-26 09:53:01 -0300

    Work OW #003
    $source=e74c685a65f65766832082594a817b1756b79cd8$

---
+++ p023-ow-003/src/main/java/post/BoxObjectosCodeExample.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2022 Objectos Software LTDA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package post;
+
+import objectos.code.JavaTemplate;
+
+public class BoxObjectosCodeExample extends JavaTemplate {
+  public @interface TypeAnnotation {}
+
+  public static void main(String[] args) {
+    System.out.println(
+      new BoxObjectosCodeExample()
+    );
+  }
+
+  @Override
+  protected final void definition() {
+    _package("com.example");
+
+    autoImports();
+
+    _class(
+      annotation(TypeAnnotation.class),
+      _public(), id("Box"),
+
+      field(
+        _private(), _final(), _int(), id("value")
+      ),
+
+      constructor(
+        _public(),
+        param(_int(), id("value")),
+        assign(n(_this(), "value"), n("value"))
+      ),
+
+      method(
+        _public(), _final(), _int(), id("get"),
+        _return(n("value"))
+      )
+    );
+  }
+}
---
+++ p023-ow-003/src/main/java/post/ChainedMethodsExample.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2022 Objectos Software LTDA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package post;
+
+import objectos.code.ClassName;
+import objectos.code.JavaTemplate;
+
+public class ChainedMethodsExample extends JavaTemplate {
+  class User {}
+
+  public static void main(String[] args) {
+    System.out.println(
+      new ChainedMethodsExample()
+    );
+  }
+
+  @Override
+  protected final void definition() {
+    _package("com.example");
+
+    autoImports();
+
+    _class(
+      _public(), id("ChainedMethods"),
+
+      method(
+        _public(), _static(), _void(), id("main"),
+        param(t(t(String.class), dim()), id("args")),
+
+        var("user", chain(
+          invoke(t(User.class), "builder"), nl(),
+          invoke("id", i(123)), nl(),
+          invoke("login", s("foo")), nl(),
+          invoke("build")
+        )),
+
+        invoke(n(ClassName.of(System.class), "out"), "println", n("user"))
+      )
+    );
+  }
+}
