458255ae2df73510e1b381086cf4f6df00b64234
Marcio Endo <marcio@objectos.com.br>
2022-12-04 09:27:48 -0300

    Update with twitter thread examples
    $source=c2319d50dfcd9407e5a629fa339319e1bb804fb1$

---
+++ p023-ow-003/src/main/java/twitter/Outer.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2022 Objectos Software LTDA.
+ *
+ * Reprodução parcial ou total proibida.
+ */
+package twitter;
+
+public class Outer {
+
+  class InnerClass {}
+
+  static class NotInnerClass {}
+
+}
