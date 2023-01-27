cca6dd622cc8e61e7a129cbc4e16bbbfb0de94e7
Marcio Endo <marcio@objectos.com.br>
2023-01-27 16:31:04 -0300

    Work Objectos Weekly #011
    $source=e8bfe912e5b89f2e9b1478aaf8f22955d4d215a4$

---
+++ p031-objectos-weekly-011/src/main/java/iter03/Create.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2022-2023 Objectos Software LTDA.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package iter03;
+
+public class Create extends Super {
+  {
+    echo("Instance initializer");
+  }
+
+  final String fieldA = initA();
+
+  public Create() {
+    echo("Create constructor");
+  }
+
+  public static void main(String[] args) {
+    new Create();
+  }
+
+  private String initA() {
+    echo("Field A initializer");
+
+    return "A";
+  }
+}
